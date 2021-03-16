65cdbf4fe73a4f5ad35b4a1ab77f76763609fa56
hbaker20 <73207344+hbaker20@users.noreply.github.com>
2021-03-16 19:48:03 -0400

    Update Polaris.java
    Do you have a preferred color for the various parts?  I randomly chose green but please feel free to change them if you'd like.  They just need to not be the defaults of blue for the body, gun, radar and scan arc or white for the bullet.

--- cscc2469_001/Polaris.java
+++ cscc2469_001/Polaris.java
@@ -1,11 +1,11 @@
 package cscc2469_001;
 import robocode.*;
-//import java.awt.Color;
+import java.awt.Color;
 
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
 /**
- * Polaris - a robot by (Helen and Tomas)
+ * Polaris - a robot by Tomas Kasparaitis and Helen Baker
  */
 public class Polaris extends Robot
 {
@@ -15,14 +15,11 @@ public class Polaris extends Robot
 	public void run() {
 		// Initialization of the robot should be put here
 
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
-
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+		// Set colors of body, gun, radar, bullet and scan arc
+		setColors(Color.green,Color.green,Color.green,Color.green,Color.green);
 
 		// Robot main loop
 		while(true) {
-			// Replace the next 4 lines with any behavior you would like
 			ahead(100);
 			turnGunRight(360);
 			back(100);
@@ -34,15 +31,15 @@ public class Polaris extends Robot
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(1);
+		if (getGunHeat() == 0) {
+			fire(1);
+		}
 	}
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
 		back(10);
 	}
 	
@@ -50,7 +47,6 @@ public class Polaris extends Robot
 	 * onHitWall: What to do when you hit a wall
 	 */
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
 		back(20);
 	}	
 }
